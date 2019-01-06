18e8fda97b1711029f71fdb91d791ff0aa844775
rail4open <43258397+rail4open@users.noreply.github.com>
2019-01-05 19:33:34 -0800

    Add files via upload

---
+++ analog/cdk_oa/MOM2D/netlsit/pre_netlist/verilog.v
@@ -0,0 +1,43 @@
+//Verilog HDL for "rail12lp", "MOM2D" "Pre_netlist"
+
+
+module MOM2D ( CB, CT, VDD, VNW, VPW, VSS );
+
+  inout CT;
+  inout VNW;
+  inout VDD;
+  inout CB;
+  inout VSS;
+  inout VPW;
+
+specify 
+    specparam CDS_LIBNAME  = "rail12lp";
+    specparam CDS_CELLNAME = "MOM2";
+    specparam CDS_VIEWNAME = "schematic";
+endspecify
+
+SC7P5T_DECAPSTX6_SSC14R I2 ( VDD, VNW, VPW, VSS);
+SC7P5T_DECAPSTX6_SSC14R I3 ( VDD, VNW, VPW, VSS);
+
+endmodule
+
+module SC7P5T_DECAPSTX6_SSC14R ( VDD, VNW, VPW, VSS );
+
+inout  VDD, VNW, VPW, VSS;
+
+
+specify 
+    specparam CDS_LIBNAME  = "IN14LPP_SC7P5T_84CPP_BASE_SSC14R";
+    specparam CDS_CELLNAME = "SC7P5T_DECAPSTX6_SSC14R";
+    specparam CDS_VIEWNAME = "schematic";
+endspecify
+
+pfet  MMP1 ( .s(VDD), .g(N0), .d(VDD));
+pfet  MMP2 ( .s(VDD), .g(N1), .d(N0));
+pfet  MMP3 ( .s(VDD), .g(N1), .d(N0));
+nfet  MMN1 ( .s(VSS), .g(N0), .d(N1));
+nfet  MMN2 ( .s(VSS), .g(N0), .d(N1));
+nfet  MMN3 ( .s(VSS), .g(N0), .d(VSS));
+nfet  MMN4 ( .s(VSS), .g(N1), .d(VSS));
+
+endmodule
